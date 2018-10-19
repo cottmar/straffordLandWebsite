e4109cb63af02fe0fb7bae748d1fdefcc2dde855
cara ottmar <cara.ottmar@gmail.com>
2018-10-18 21:59:36 -0700

    working on routes

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router';
-import { BrowserRouter, Link } from 'react-router-dom';
-import Info from '../Info/info';
-import Map from '../Map/map';
-import Footer from '../Footer/footer';
+import React from 'react';
+// import { Route } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import Info from '../Info/info';
+// import Map from '../Map/map';
+// import Footer from '../Footer/footer';
 import Header from '../Header/header';
 import Forty from '../40/forty';
 import SeventyFive from '../75/seventyFive';
@@ -12,26 +12,10 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <div id='navBar'>
       <Header/>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-      {/* <Route exact path="/" component={Home} /> */}
-        </li>
-        <li>
-          <Link to="/Forty">Forty Acres</Link>
-          <Route path="/Forty" component={Forty}/>
-        </li>
-        <li>
-          <Link to="/SeventyFive">Seventy Five Acres</Link>
-          <Route path="/SeventyFive" component={SeventyFive}/>
-        </li>
-      </ul>
-          <Info />
-          <Map/>
-          <Footer/>
-      </div> 
+      <Route exact path='/' component={App}/>
+      <Route exact path='/forty' component={Forty}/>
+      <Route exact path='/seventyFive' component={SeventyFive}/>
       </BrowserRouter>
     );
   }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
@@ -6,6 +7,12 @@ class Header extends Component {
     <div id="header">
       <div id="words">A C R E A G E&emsp;F O R&emsp;S A L E
       </div>
+    <nav>
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/forty'>Forty</Link></li>
+          <li><Link to='/projects'>Seventy Five</Link></li>
+          <li><Link to='/contact'>CONTACT</Link></li>
+    </nav>
     </div>
     )
   }
