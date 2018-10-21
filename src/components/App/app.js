88cbde0195c21e09cfa1a28fcb24f493e0758e7b
cara ottmar <cara.ottmar@gmail.com>
2018-10-20 18:06:54 -0700

    working on landing page

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import { Router, Route} from 'react-router';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Info from '../Info/info';
 import Map from '../Map/map';
 import Footer from '../Footer/footer';
 import Header from '../Header/header';
 import Forty from '../40/forty';
 import SeventyFive from '../75/seventyFive';
+import Landing from '../../components/landing/landing';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div id='navBar'>
+      <div id='BrowserRoutes'>
       <Header/>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          {/* <Link to="/">Home</Link> */}
         </li>
         <li>
-          <Link to="/Forty">Forty Acres</Link>
+          {/* <Link to="/Forty">Forty Acres</Link> */}
           <Route path="/Forty" component={Forty}/  >
         </li>
         <li>
-          <Link to="/SeventyFive">Seventy Five Acres</Link>
+          {/* <Link to="/SeventyFive">Seventy Five Acres</Link> */}
           <Route path="/SeventyFive" component={SeventyFive}/>
         </li>
       </ul>
       {/* <Route exact path="/" component={Home} /> */}
           <Info />
+          <Landing/>
           <Map/>
           <Footer/>
       </div> 
---
+++ src/components/landing/landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className='landing'>
+          {/* <Link to="/">Home</Link>
+          <Link to="/Forty">Forty Acres</Link> */}
+
+          <p>DSJKGKLSDJFKL</p>
+      </div>
+
+    )
+  }
+}
+
+export default Landing;
