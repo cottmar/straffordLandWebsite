230d2800e50e460c9e997c2ac6755bbc497c7b8b
cara ottmar <cara.ottmar@gmail.com>
2018-10-20 17:14:04 -0700

    back to rendering

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,21 +1,37 @@
-import React from 'react';
-// import { Route } from 'react-router';
-import { BrowserRouter, Route } from 'react-router-dom';
-// import Info from '../Info/info';
-// import Map from '../Map/map';
-// import Footer from '../Footer/footer';
+import React, { Component } from 'react';
+import { Router, Route} from 'react-router';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Info from '../Info/info';
+import Map from '../Map/map';
+import Footer from '../Footer/footer';
 import Header from '../Header/header';
 import Forty from '../40/forty';
 import SeventyFive from '../75/seventyFive';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <div id='navBar'>
       <Header/>
-      <Route exact path='/' component={App}/>
-      <Route exact path='/forty' component={Forty}/>
-      <Route exact path='/seventyFive' component={SeventyFive}/>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Forty">Forty Acres</Link>
+          <Route path="/Forty" component={Forty}/  >
+        </li>
+        <li>
+          <Link to="/SeventyFive">Seventy Five Acres</Link>
+          <Route path="/SeventyFive" component={SeventyFive}/>
+        </li>
+      </ul>
+      {/* <Route exact path="/" component={Home} /> */}
+          <Info />
+          <Map/>
+          <Footer/>
+      </div> 
       </BrowserRouter>
     );
   }
