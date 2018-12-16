352076e3353bfd14bb3d8913550eabb87c2f59b0
cara ottmar <cara.ottmar@gmail.com>
2018-12-16 15:07:14 -0800

    testing tweaks after deployment

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -12,20 +12,23 @@ class Info extends Component {
       <p>Strafford, Missouri</p>
       </div>
         <div className="details">
-        Peaceful plot of 75 acres close to I-44 and convenient to Springfield. Property is located off Stan Harriman Blvd, situated on the North side between Vermillion and Old Orchard. 
+        <ul>Peaceful plot of 75 acres close to I-44 and convenient to Springfield. Property is located off Stan Harriman Blvd, situated on the North side between Vermillion and Old Orchard. 
         <br></br>
+        </ul>
+        <ul>
         City of Strafford planning a 12-inch water main on the north side of Stan Harriman Blvd between the Fire Station and Vermillion Drive.
         <br></br>
+        </ul>
+        <ul>
         City owned and operated Sanitary Sewer Lift Station is located in the Southeast corner of the intersection of Old Orchard Drive and Stan Harriman Blvd.
         <br></br>
-        $8,000/acre
+        </ul>
+        <ul>
+        $8,000 per acre.
         For more information, contact Duane Ottmar (phone number, email) 
         {this.props.test}
+        </ul>
         </div>
-        {/* <div className='links'>
-        <Link to='/forty'><button>Forty Acres</button></Link>
-        <Link to='/seventyFive'><button>Seventy Five Acres</button></Link>
-        </div> */}
         <Map/>
       </div>
     );
